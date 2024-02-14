b8695d7a1ea04cc9fa1116080dde5c8965c98389
Gonza-DH <gonzadigitalhouse@gmail.com>
2024-02-14 20:03:12 -0300

    Se inicializo el proyecto con node y express

---
+++ app.js
@@ -0,0 +1,14 @@
+const express = require ('express');
+const app = express();
+const port = 3030;
+const path = require ('path');
+
+app.use(express.static('public'));
+
+app.get('/', (req,res) => {
+    res.sendFile(path.join(__dirname,'/views/index.html'));
+});
+
+
+app.listen(port, () => 
+    console.log('Servidor online en el puerto '+port));
