177f4ace0b0e4671ad21a353a4c8ed9461d35198
MiguelNicolasGonzalez <nicolitonico17@hotmail.es>
2024-02-26 19:00:02 -0300

    carrito de compras modificar

--- app.js
+++ app.js
@@ -9,6 +9,9 @@ app.get('/', (req,res) => {
     res.sendFile(path.join(__dirname,'/views/index.html'));
 });
 
+app.get('/productCart', (req,res) => {
+    res.sendFile(path.join(__dirname, './views/productCart.html'));      /*agregar ruta*/
+});
 
 app.listen(port, () => 
     console.log('Servidor online en el puerto '+port));
