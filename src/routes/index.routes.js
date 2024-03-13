355e6cfd76d46646e478578a5188e8df8ef162bf
Gonza-DH <gonzadigitalhouse@gmail.com>
2024-03-12 22:19:02 -0300

    Se hicieron dinamicas las vistas de index

--- src/routes/index.routes.js
+++ src/routes/index.routes.js
@@ -1,12 +1,12 @@
 const express = require('express');
 const path = require('path');
 const routes = express.Router();
+const productos = require('../models/productsList.json')
 
 const productRouter = require('./products.routes');
 const userRouter = require('./users.routes');
 
-// routes.get('/', (req,res) => res.sendFile(path.resolve(__dirname, '../views/index.html')));
-routes.get('/', (req,res) => res.render("index"));
+routes.get('/', (req,res) => res.render("index", {products: productos}));
 routes.use('/producto', productRouter);
 routes.use('/cuenta', userRouter);
 
