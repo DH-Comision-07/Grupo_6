9c0eb039782bc4eda5605536291a9c3d6cd2add7
MiguelNicolasGonzalez <nicolitonico17@hotmail.es>
2024-03-12 21:44:38 -0300

    modificamos rutas archivos creacion y edicion

--- src/controllers/productsController.js
+++ src/controllers/productsController.js
@@ -1,10 +1,16 @@
 const express = require('express');
 const path = require('path');
+let productos = require('../models/productsList.json');
+
 
 const productsController = { 
     getDetail: (req,res) => res.render("products/productDetail"),    
     
     getCart: (req,res) => res.render("products/productCart"),
+
+    getNew: (req,res) => res.render("products/productCreate"),
+
+    getEditBy: (req,res) => res.render("products/productEdit", {product: productos.find ((producto) => producto.id==req.params.id)})
     
 };
 
