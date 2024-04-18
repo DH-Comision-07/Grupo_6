24548fa6d1c5fa092ec1e1ca303a7245181ee896
ramirez-leylen <ramirezleylen@gmail.com>
2024-04-18 20:42:40 -0300

    se agrego registro de usuarios robusto

--- src/controllers/usersController.js
+++ src/controllers/usersController.js
@@ -1,13 +1,15 @@
 const express = require('express');
 const path = require('path');
 
+let userService = require('../data/userService');
+
 const usersController = { 
     login: (req,res) => res.render("users/login.ejs"),
 
     register: (req, res) => res.render("users/register.ejs"),
 
     storeRegister: (req,res) => {
-        // userService.store(req.body);
+        userService.store(req.body);
 		res.redirect('/');
     },
 }
---
+++ src/data/userService.js
@@ -0,0 +1,24 @@
+const fs = require('fs');
+const path = require('path');
+
+const usersFilePath = path.join(__dirname, './users.json');
+const users = JSON.parse(fs.readFileSync(usersFilePath, 'utf-8'));
+
+let usersService = {
+    store: function(user) {
+        let newUser = {
+            id: users[users.length - 1].id + 1, // crea id, ARREGLAR?
+            firstName: user.firstName.toLowerCase(),
+            lastName: user.lastName.toLowerCase(),
+            email: user.email,
+            password: user.password,   // ENCRIPTAR !!
+            type: user.type
+            // avatar: "/images/users/"+image.filename,
+        };
+
+        users.push(newUser);
+        fs.writeFileSync(usersFilePath, JSON.stringify(users));
+    },
+};
+
+module.exports = usersService;
