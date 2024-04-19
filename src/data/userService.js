f2691fd90cca5969e88301f5e17ad7d57162ff62
ramirez-leylen <ramirezleylen@gmail.com>
2024-04-19 15:58:59 -0300

    agregue hasheado de contrasena, pedido de username en el registro y mejore el multer

--- src/data/userService.js
+++ src/data/userService.js
@@ -1,6 +1,9 @@
 const fs = require('fs');
 const path = require('path');
 
+const bcrypt = require("bcryptjs");
+const salt = 12;
+
 const usersFilePath = path.join(__dirname, './users.json');
 const users = JSON.parse(fs.readFileSync(usersFilePath, 'utf-8'));
 
@@ -10,8 +13,9 @@ let usersService = {
             id: users[users.length - 1].id + 1, // crea id, ARREGLAR?
             firstName: user.firstName.toLowerCase(),
             lastName: user.lastName.toLowerCase(),
+            username: user.username,
             email: user.email,
-            password: user.password,   // ENCRIPTAR !!
+            password: bcrypt.hashSync(user.password, salt),
             type: user.type,
             avatar: "/images/users/"+avatar.filename,
         };
--- src/routes/users.routes.js
+++ src/routes/users.routes.js
@@ -8,7 +8,7 @@ const storage = multer.diskStorage({
         cb(null, "public/images/users")
     },
     filename: function(req,file,cb) {
-        cb(null, "img-"+req.body.firstName.toLowerCase().replace(/\s/g, "-")+req.body.lastName.toLowerCase().replace(/\s/g, "-")+path.extname(file.originalname))
+        cb(null, "img-"+(req.body.username).toLowerCase().replace(/\s/g, "-").replace(".", "")+path.extname(file.originalname))
     }
 })
 const uploadFile = multer({storage: storage});
