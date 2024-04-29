bf65c8bd28ee29156ec5359f4c3ac5296449ecc3
Gonza-DH <gonzadigitalhouse@gmail.com>
2024-04-29 12:41:13 -0300

    verificando rutas huespedes

--- src/middlewares/guestOnly.js
+++ src/middlewares/guestOnly.js
@@ -1,6 +1,6 @@
 function guestOnly(req,res,next) {
     if (req.session.isLogged == true) {
-        res.redirect("/")  // rutas solo para no logeados llevan a home, no hya perfil
+        res.redirect("/")  
     } else {
         next()
     }
